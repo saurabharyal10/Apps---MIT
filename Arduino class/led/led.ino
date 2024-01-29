e3a334c48d1573b1f05f0728b1821ab79689d310
Saurabh Aryal <saurabharyal10@gmail.com>
2024-01-29 12:19:43 +0545

    Initial Commit

---
+++ Arduino class/led/led.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2,1);
+  digitalWrite(3,1);
+  digitalWrite(4,1);
+  digitalWrite(5,1);
+  digitalWrite(6,1);
+  digitalWrite(7,1);
+  digitalWrite(8,1);
+  digitalWrite(9,1);
+  delay(1000);
+}
