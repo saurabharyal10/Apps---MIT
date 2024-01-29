be68906d4496a224855e2e4b171d4a49e968a79a
Saurabh Aryal <saurabharyal10@gmail.com>
2024-01-29 12:19:24 +0545

    Initial Commit

---
+++ Arduino class/PIR_Sensor/PIR_Sensor.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+  pinMode(9,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(9)==HIGH)
+    digitalWrite(13,HIGH);
+  else
+    digitalWrite(13,LOW);
+  
+}
