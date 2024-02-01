0afda5909ade8a0f066a506e6814a3182e23a4bf
Saurabh Aryal <saurabharyal10@gmail.com>
2024-02-01 08:37:11 +0545

    Initial Commit

---
+++ Arduino class/segment_display1/segment_display1.ino
@@ -0,0 +1,97 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+    digitalWrite(2,0);
+    digitalWrite(3,1);
+    digitalWrite(4,1);
+    digitalWrite(5,0);
+    digitalWrite(6,0);
+    digitalWrite(7,0);
+    digitalWrite(8,0);   
+    delay(500);
+    digitalWrite(2,1);
+    digitalWrite(3,1);
+    digitalWrite(4,0);
+    digitalWrite(5,1);
+    digitalWrite(6,1);
+    digitalWrite(7,0);
+    digitalWrite(8,1);   
+    delay(500);
+    digitalWrite(2,1);
+    digitalWrite(3,1);
+    digitalWrite(4,1);
+    digitalWrite(5,1);
+    digitalWrite(6,0);
+    digitalWrite(7,0);
+    digitalWrite(8,1);   
+    delay(500);
+    digitalWrite(2,0);
+    digitalWrite(3,1);
+    digitalWrite(4,1);
+    digitalWrite(5,0);
+    digitalWrite(6,0);
+    digitalWrite(7,1);
+    digitalWrite(8,1);   
+    delay(500);
+    digitalWrite(2,1);
+    digitalWrite(3,0);
+    digitalWrite(4,1);
+    digitalWrite(5,1);
+    digitalWrite(6,0);
+    digitalWrite(7,1);
+    digitalWrite(8,1);   
+    delay(500);
+    digitalWrite(2,1);
+    digitalWrite(3,0);
+    digitalWrite(4,1);
+    digitalWrite(5,1);
+    digitalWrite(6,1);
+    digitalWrite(7,1);
+    digitalWrite(8,1);   
+    delay(500);
+    digitalWrite(2,1);
+    digitalWrite(3,1);
+    digitalWrite(4,1);
+    digitalWrite(5,0);
+    digitalWrite(6,0);
+    digitalWrite(7,0);
+    digitalWrite(8,0);   
+    delay(500);
+    digitalWrite(2,1);
+    digitalWrite(3,1);
+    digitalWrite(4,1);
+    digitalWrite(5,1);
+    digitalWrite(6,1);
+    digitalWrite(7,1);
+    digitalWrite(8,1);   
+    delay(500);
+    digitalWrite(2,1);
+    digitalWrite(3,1);
+    digitalWrite(4,1);
+    digitalWrite(5,1);
+    digitalWrite(6,0);
+    digitalWrite(7,1);
+    digitalWrite(8,1);   
+    delay(500);
+    digitalWrite(2,1);
+    digitalWrite(3,1);
+    digitalWrite(4,1);
+    digitalWrite(5,1);
+    digitalWrite(6,1);
+    digitalWrite(7,1);
+    digitalWrite(8,0);   
+    delay(500);
+ 
+}
